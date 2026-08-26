faa55dd254c52750052ec3cf04f5aadadeed58bd
William Ezzell <ezzellmax@gmail.com>
2026-08-26 15:19:40 -0400

    Finished Lab 2

--- Lab_2/fastq_qc.py
+++ Lab_2/fastq_qc.py
@@ -20,19 +20,22 @@ def main(path):
             seq = fh.readline().strip()
             plus = fh.readline()
             qual = fh.readline().strip()
-            # TODO: increment n (one record was read)
+            #Increments n (one record was read)
             n+=1
-            # TODO: add len(seq) to total_len
+            
+            #Add the length of seq  to total_len
             total_len+=len(seq)
-            # TODO: add the sum of Phred scores of qual to total_qual
-            total_qual+=ord(qual) - 33)
-            #       (a Phred score is ord(char) - 33)
-            # TODO: add len(qual) to total_qbases
+
+            # Loop through qual then add the sum of Phred scores (ord(char)-33) to total_qual
+            total_qual += sum(ord(c) - 33 for c in qual)
+            
+            # Add the length of qual to total_qbases
             total_qbases+=len(qual)
     print(f"reads: {n}")
-    # TODO: print "mean read length: X" with X = total_len / n, one decimal
-    # TODO: print "mean quality (Phred): X" with X = total_qual / total_qbases, two decimals
-
+    
+    print(f"mean read length: {total_len / n:.1f}")
+    
+    print(f"mean quality (Phred): {total_qual / total_qbases:.2f}")
 
 if __name__ == "__main__":
     main(sys.argv[1])
